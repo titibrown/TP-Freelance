5be0e783cdac4fe46027fe7e8e3cf07ac455da37
Tim <timothybrown@free.fr>
2021-01-07 14:57:33 +0100

    Add project files.

---
+++ Freelance/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freelance.Models
+{
+    public class Customer
+    {
+    }
+}
---
+++ Freelance/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Freelance.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
