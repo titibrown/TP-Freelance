b854b67654f4316684d5929c9e758b84d17b7425
Tim <timothybrown@free.fr>
2021-01-07 15:13:15 +0100

    Je sors la tête de l'eau

--- Freelance/Models/Customer.cs
+++ Freelance/Models/Customer.cs
@@ -7,5 +7,12 @@ namespace Freelance.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+
+        public int CatId { get; set; }
+
+
     }
 }
