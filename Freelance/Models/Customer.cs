322846d8dc91dcb9aa04552e1287e139ab7f716b
Tim <timothybrown@free.fr>
2021-01-11 14:43:32 +0100

    Model Customer

--- Freelance/Models/Customer.cs
+++ Freelance/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,39 @@ namespace Freelance.Models
 {
     public class Customer
     {
-        public int Id { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerEmail { get; set; }
-
-        public int CatId { get; set; }
 
 
-    }
+
+        [Key]
+        [Column("customer_id")]
+        public int ID
+        {
+            get; set;
+        }
+
+        [Required]
+        [Column("customer_name")]
+        [StringLenght(100)]
+        [DataType(DataType.Text)]
+        public string Name 
+        { 
+            get; set; 
+        }
+
+        [Required]
+        [Column("customer_email")]
+        [StringLenght(100)]
+        [DataType(DataType.EmailAddress)]
+
+        public string email
+        {
+            get; set;
+        }
+
+
+
+
+
+
+}
 }
