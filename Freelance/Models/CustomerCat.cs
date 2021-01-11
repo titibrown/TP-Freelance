dce6a6f4c1845aaaa2da2f3b4f2a3dd8e187ff87
Tim <timothybrown@free.fr>
2021-01-11 15:02:34 +0100

    Continue

--- Freelance/Models/CustomerCat.cs
+++ Freelance/Models/CustomerCat.cs
@@ -1,11 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Freelance.Models
 {
+    [Table("customer_cats")]
     public class CustomerCat
     {
+        [Key]
+        [Column("cat_id")]
+        public int ID
+        {
+            get; set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string Name
+        {
+            get; set;
+        }
+
+
+        public string Description 
+
     }
 }
--- Freelance/Models/Job.cs
+++ Freelance/Models/Job.cs
@@ -1,11 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Freelance.Models
 {
+    [Table("jobs")]
     public class Job
     {
+
+       
+      
+        [Key]
+        [Column("job_id")]
+        public int ID
+        {
+            get; set;
+        }
+
+        public string Job_State
+        {
+            get; set;
+        }
+
+        public string Job_Title
+        {
+            get; set;
+        }
+
+
+          [DataType(DataType.Date)]
+          public DateTime Start
+        {
+            get; set;
+        }
+
+        [DataType(DataType.Date)]
+        public DateTime End
+        {
+            get; set;
+        }
+
+        public string Job_Description
+        {
+            get; set;
+        }
+
+
+
+
+
+
+
+
+        }
+
+
     }
-}
+
